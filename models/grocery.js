3d54ad7789b92354674f4941927a15dfcbb4b660
dev-rahulbhadoriya <rbhadoria97@gmail.com>
2024-02-03 23:29:40 +0530

    ADD: grocery model

--- models/grocery.js
+++ models/grocery.js
@@ -1,1 +1,45 @@
-//TODO : grocery model
+
+const {
+    Model
+  } = require('sequelize');
+
+  module.exports = (sequelize, DataTypes) => {
+    class grocery extends Model {
+      static associate(models) {
+
+      }
+    }
+    grocery.init({
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+            unique: true,
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+          },
+          quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+          },
+    }, {
+      sequelize,
+      modelName: 'grocery',
+    });
+  
+  
+    return grocery;
+  };
+  
+
